b1c0dfed586dfad03fa60ac5062bb022c0b0ffca
cesco <cescofors75@gmail.com>
2025-05-05 15:56:32 +0200

    clean menu

--- components/menu-preview.tsx
+++ components/menu-preview.tsx
@@ -189,17 +189,7 @@ export default function MenuPreview() {
             )}
           </div>
 
-          <div className="mt-12 text-center">
-            <Link 
-              href="/menu"
-              className="inline-flex items-center px-6 py-3 bg-brand text-white rounded-md font-medium hover:bg-brand/90 transition-colors"
-            >
-              {t("menu.view_full_menu")}
-              <svg xmlns="http://www.w3.org/2000/svg" className="ml-2 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
-              </svg>
-            </Link>
-          </div>
+          
         </div>
       </div>
     </section>
